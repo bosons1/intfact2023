b9461dc555fabcb21c15080321f0fba47ecb8b0b
Subhendra Basu <basu.subhendra@outlook.com>
2023-07-03 19:00:46 +0530

    code

--- intfact.py
+++ intfact.py
@@ -1,72 +1,46 @@
 #!/usr/bin/python3
 import sys
-from mpmath import *
 from gmpy2 import *
 
-def satisfies(degree, digit):
-    f=open("./pi.txt", "r")
-    g=open("./sqrt2.txt","r")
-    c = 0
-    f.read(2)
-    g.read(2)
-    pp = ""
-    tt = ""
-    while True:
-        pp = str(f.read(1))
-        tt = str(g.read(1))
-        if pp == str(digit):
-            c = c + 1
-            if c == degree:
-                break
-    f.close()
-    g.close()
-    d = int(pp) + int(tt)
-    if d == 1 or d == 11:
-        return True
-    else:
-        return False
+def complement(num):
+    c_num = ""
+    for x in num:
+        c = str((11 - int(x)) % 10)
+        c_num = c_num + c
+    return c_num
 
-def divides(num, factor):
-    fz = mpz(str(factor))
-    nz = mpz(num)
-    if fz <= 1:
-        return False, ""
-    rz = fmod(nz, fz)
-    if rz == 0:
-        qz = fdiv(nz, fz)
-        return True, str(qz)
-    else:
-        return False, ""
+def characterize(num):
+    c = 0
+    f = open("./sqrt2.txt", "r")
+    state_vec = []
+    f.read(2)
+    l = len(num)
+    pos = 0
+    while c < l:
+        s = str(f.read(1))
+        d = int(s) + int(num[pos % l])
+        if d == 11 or d == 1:
+            state_vec.append(1)
+            c = c + 1
+        else:
+            state_vec.append(0)
+        pos = pos + 1
+    f.close()
+    return state_vec
 
 num=str(sys.argv[1])
 l = len(num)
-zero_index = 1
-mp.prec=27
-mp.dps=27
-i = 0
-f=open("./pi.txt","r")
-g=open("./sqrt2.txt","r")
-f.read(2)
-g.read(2)
-while True:
-   zero = str(zetazero(zero_index).imag)
-   idx = zero.index(".")
-   zero = zero[idx + 1:]
-   zero = zero[:8]
-   nn = num[l - 1 - i]
-   i = (i + 1) % l
-   hit = False
-   zero_index = zero_index + 1
-   for digit in zero:
-       if digit == '0':
-           digit = 10
-       if not hit and satisfies(int(digit), int(nn)) == True:
-           hit = True
-   pp = str(f.read(1))
-   tt = str(g.read(1))
-   print([pp, tt])
-   if hit == True:
-       input("hit")
-   print("")
-f.close()
-g.close()
+state_vec1 = characterize(num)
+c_num = complement(num)
+state_vec2 = characterize(c_num)
+print(len(state_vec1), len(state_vec2))
+"""
+factor1 = factorize(state_vec1)
+factor2 = factorize(state_vec2)
+dec1 = int(factor1, 2)
+dec2 = int(factor2, 2)
+if dec1*dec2 == num:
+    print(num + " = " + str(dec1) + " X " + str(dec2))
+else:
+    print(num + " is a Prime Number.")
+    """
